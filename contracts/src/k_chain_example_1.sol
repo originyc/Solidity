f28efb3314f9f148ddc0756ccc93018b6fddb8f0
Eric Rubin <102687769+originyc@users.noreply.github.com>
2022-04-18 22:15:12 -0400

    Create k_chain_example_1.sol

---
+++ contracts/src/k_chain_example_1.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract subtract{
+    function subtracting(uint x, uint y) external pure returns(uint){
+        return x - y;
+    }
+}
+
