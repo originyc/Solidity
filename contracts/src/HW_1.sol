446b4dd43eae2973a4de300fa705706a2dcd29aa
Eric Rubin <102687769+originyc@users.noreply.github.com>
2022-04-18 22:14:15 -0400

    Create HW_1.sol

---
+++ contracts/src/HW_1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+contract localVariables{
+    string public name = "0";
+    uint public i;
+
+    function fizzbuzz() external{
+       
+        i = 2892;
+        name = "happyMeal";
+    }
+}
